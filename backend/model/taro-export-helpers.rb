325be1cde03ad38316b17da5f8ec3c23ac4c18e7
Brian Harrington <brian.harrington@lyrasis.org>
2024-07-22 14:01:30 -0400

    Updates based on testing feedback

--- backend/model/taro-export-helpers.rb
+++ backend/model/taro-export-helpers.rb
@@ -96,5 +96,44 @@ module ASpaceExport
       @controlaccess_subjects
     end
 
+		def archdesc_dates
+      unless @archdesc_dates
+        results = []
+        dates = self.dates || []
+        dates.each do |date|
+          normal = ""
+          unless date['begin'].nil?
+            normal = "#{date['begin']}/"
+            normal_suffix = (date['date_type'] == 'single' || date['end'].nil? || date['end'] == date['begin']) ? date['begin'] : date['end']
+            normal += normal_suffix ? normal_suffix : ""
+          end
+          type = ( date['date_type'] == 'inclusive' ) ? 'inclusive' : ( ( date['date_type'] == 'single') ? nil : 'bulk')
+          content = if date['expression']
+                      date['expression']
+                    elsif date['end'].nil? || date['end'] == date['begin']
+                      date['begin']
+                    else
+                      "#{date['begin']}-#{date['end']}"
+                    end
+
+          atts = {}
+          atts[:type] = type if type
+          atts[:certainty] = date['certainty'] if date['certainty']
+          atts[:normal] = normal unless normal.empty?
+          atts[:era] = date['era'].nil? ? 'ce' : date['era']
+          atts[:calendar] = date['calendar'].nil? ? 'gregorian' : date['calendar']
+          atts[:datechar] = date['label'] if date['label']
+					atts[:encodinganalog] = date['date_type'] == 'bulk' ? '245$g' : '245$f'
+
+          results << {:content => content, :atts => atts}
+        end
+
+        @archdesc_dates = results
+      end
+
+      @archdesc_dates
+    end
+
+
 	end
 end
